14773d925712bc8a3e31f893902b0cb0aa1165ff
Pomin <pominx+github@gmail.com>
2012-12-13 16:21:08 +0800

    Working user signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Return a title on a per-page basis.
-  def title
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
-  end
-  
   protect_from_forgery
+  include SessionsHelper
 end
